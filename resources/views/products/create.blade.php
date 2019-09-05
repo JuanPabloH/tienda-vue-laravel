12ea6f6d185a6a8a9f1b1ee1a6e03b7dabd1546f
JuanPabloH <jpherrera69@gmail.com>
2019-09-05 11:35:32 -0500

    Formulario para agregar un producto terminado

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        
+     <add-product-component></add-product-component>   
+                        
+    </div>
+</div>
+@endsection
